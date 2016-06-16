65f7e95b40574efbd787d73f490465cd2bae0fcc
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-06-16 10:41:58 -0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,16 +3,37 @@
 # fix 'stdin: is not a tty' issue
 sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 
-apt-get update
+apt-get update && apt-get install -y \
+	apt-utils \
+	automake \
+	zsh \
+	bash-completion \
+	binutils-mingw-w64 \
+	build-essential \
+	clang \
+	curl \
+	dpkg-sig \
+	gcc-mingw-w64 \
+	git-core \
+	git \
+	libcap-dev \
+	libltdl-dev \
+	libtool \
+	libssl-dev \
+	net-tools \
+	pkg-config \
+	python-dev \
+	python-mock \
+	python-pip \
+	python-websocket \
+	tar \
+	postgresql-client-9.3 \
+	zip
 
-apt-get install git-core -qq
+su - vagrant -c "wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh"
 
-apt-get install zsh -qq
+chsh -s `which zsh` vagrant
 
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-
-sudo chsh -s `which zsh`
-
-sudo apt-get install postgresql-client-9.3 -qq
+su - vagrant -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | zsh"
 
 exit 0
